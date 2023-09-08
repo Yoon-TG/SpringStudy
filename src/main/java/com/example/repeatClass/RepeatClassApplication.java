4d739af1bec66d201a48635e0e0f4e9e1e5357f3
Yoon-TG <witteny24@gmail.com>
2023-09-08 15:34:42 +0900

    0_기본 설정 완료 코드

---
+++ src/main/java/com/example/repeatClass/RepeatClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.repeatClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatClassApplication.class, args);
+	}
+
+}
